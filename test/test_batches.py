0086f267c81f67050df80ce68e57e54b4ae7e06b
Ajahn Jhanarato <jhanarato@gmail.com>
2023-10-02 12:53:33 +0800

    Species names are constants.

--- test/test_batches.py
+++ test/test_batches.py
@@ -5,9 +5,14 @@ import pytest
 from revegetator.domain.model import Batch, Stock
 
 
+BANKSIA = "Banksia littoralis"
+HAKEA = "Hakea varia"
+HYPOCALYMNA = "Hypocalymma angustifolium"
+
+
 @pytest.fixture
 def species_names():
-    return ["Banksia littoralis", "Hakea varia", "Hypocalymma angustifolium"]
+    return [BANKSIA, HAKEA, HYPOCALYMNA]
 
 
 @pytest.fixture
@@ -35,14 +40,14 @@ def batch_of_three(batch, species_names):
 
 def test_should_combine_quantities_of_same_stock(batch):
     stock = Stock(
-        species="Banksia littoralis",
+        species=BANKSIA,
         quantity=20,
         size="tube"
     )
 
     batch.add(stock)
     batch.add(stock)
-    assert batch.quantity("Banksia littoralis") == 40
+    assert batch.quantity(BANKSIA) == 40
 
 
 def test_should_get_species_list_from_batch(species_names, batch_of_three):
@@ -50,13 +55,13 @@ def test_should_get_species_list_from_batch(species_names, batch_of_three):
 
 
 def test_should_get_species_quantity(batch_of_three):
-    assert batch_of_three.quantity("Banksia littoralis") == 20
+    assert batch_of_three.quantity(BANKSIA) == 20
 
 
 def test_should_give_quantity_of_sized_stock(batch):
     batch.add(
         Stock(
-            species="Banksia littoralis",
+            species=BANKSIA,
             quantity=20,
             size="tube"
         )
@@ -64,11 +69,11 @@ def test_should_give_quantity_of_sized_stock(batch):
 
     batch.add(
         Stock(
-            species="Banksia littoralis",
+            species=BANKSIA,
             quantity=10,
             size="pot"
         )
     )
 
-    assert batch.quantity("Banksia littoralis", size="tube") == 20
-    assert batch.quantity("Banksia littoralis", size="pot") == 10
+    assert batch.quantity(BANKSIA, size="tube") == 20
+    assert batch.quantity(BANKSIA, size="pot") == 10
