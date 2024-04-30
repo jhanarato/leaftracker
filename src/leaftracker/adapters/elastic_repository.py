d4630bc37d09cf83640687e0e7b5c09afe3c83c5
Ajahn Jhanarato <jhanarato@gmail.com>
2024-04-30 09:26:48 +0800

    Elasticsearch repository stores species.

--- src/leaftracker/adapters/elastic_repository.py
+++ src/leaftracker/adapters/elastic_repository.py
@@ -1,9 +1,22 @@
+from elasticsearch import Elasticsearch
+
 from leaftracker.domain.model import Species
 
 
 class SpeciesRepository:
+    def __init__(self):
+        self._client = Elasticsearch(hosts="http://localhost:9200")
+
     def add(self, species: Species) -> str:
-        pass
+        self._client.index(
+            index="species",
+            id=species.reference,
+            document={
+                "genus": species.names[0].genus,
+                "species": species.names[0].genus,
+            }
+        )
+        return species.reference
 
     def get(self, species_ref: str) -> Species:
         pass
--- test/test_elastic_repository.py
+++ test/test_elastic_repository.py
@@ -1,12 +1,20 @@
+from elasticsearch import Elasticsearch
+
+from leaftracker.adapters.elastic_repository import SpeciesRepository
 from leaftracker.domain.model import Species, ScientificName
 
 
 class TestSpeciesRepository:
-    def test_add(self):
+    def test_should_add_a_species(self):
         name = ScientificName(
             genus="Acacia",
             species="Saligna",
             is_most_recent=True
         )
 
-        species = Species("acacia-saligna", name)
+        species_in = Species("acacia-saligna", name)
+        repo = SpeciesRepository()
+        repo.add(species_in)
+
+        es = Elasticsearch(hosts="http://localhost:9200")
+        assert es.exists(index="species", id="acacia-saligna")
