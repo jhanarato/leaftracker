f62afdff820cd77f3470e217b525dc189853f284
Ajahn Jhanarato <jhanarato@gmail.com>
2024-04-30 10:10:20 +0800

    Introduce species fixture.

--- test/test_elastic_repository.py
+++ test/test_elastic_repository.py
@@ -1,23 +1,28 @@
+import pytest
 from elasticsearch import Elasticsearch
 
 from leaftracker.adapters.elastic_repository import SpeciesRepository
 from leaftracker.domain.model import Species, ScientificName
 
 
+@pytest.fixture
+def acacia_species() -> Species:
+    name = ScientificName(
+        genus="Acacia",
+        species="Saligna",
+        is_most_recent=True
+    )
+
+    return Species("acacia-saligna", name)
+
+
 class TestSpeciesRepository:
-    def test_should_add_a_species(self):
+    def test_should_add_a_species(self, acacia_species):
         es = Elasticsearch(hosts="http://localhost:9200")
         assert es.indices.exists(index="species")
-        es.delete(index="species", id="acacia-saligna")
+        es.delete(index="species", id=acacia_species.reference)
 
-        name = ScientificName(
-            genus="Acacia",
-            species="Saligna",
-            is_most_recent=True
-        )
-
-        species_in = Species("acacia-saligna", name)
         repo = SpeciesRepository()
-        repo.add(species_in)
+        repo.add(acacia_species)
 
         assert es.exists(index="species", id="acacia-saligna")
