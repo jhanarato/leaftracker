50eb649fabd9ca76fc7c7786b146c9843245ed45
Ajahn Jhanarato <jhanarato@gmail.com>
2024-05-05 10:20:52 +0800

    Tests gets index from repository.

--- test/test_elasticsearch.py
+++ test/test_elasticsearch.py
@@ -32,26 +32,26 @@ def new_repo() -> SpeciesRepository:
 
 class TestSpeciesRepository:
     def test_should_create_new_index(self, es):
-        es.options(ignore_status=404).indices.delete(index="species")
         repo = SpeciesRepository()
+        es.options(ignore_status=404).indices.delete(index=repo.index)
         repo.create_index()
-        assert es.indices.exists(index="species")
+        assert es.indices.exists(index=repo.index)
 
     def test_should_delete_missing_index(self, es):
         repo = SpeciesRepository()
         repo.delete_index()
         repo.delete_index()
-        assert not es.indices.exists(index="species")
+        assert not es.indices.exists(index=repo.index)
 
     def test_should_delete_existing_index(self, es):
         repo = SpeciesRepository()
         repo.create_index()
         repo.delete_index()
-        assert not es.indices.exists(index="species")
+        assert not es.indices.exists(index=repo.index)
 
     def test_should_add_a_species(self, new_repo, acacia, es):
         reference = new_repo.add(Species("acacia-saligna", acacia))
-        assert es.exists(index="species", id=reference)
+        assert es.exists(index=new_repo.index, id=reference)
 
     def test_should_get_a_species(self, new_repo, acacia):
         species_in = Species("acacia-saligna", acacia)
@@ -67,8 +67,8 @@ class TestSpeciesRepository:
     def test_should_delete_all_documents(self, new_repo, acacia, es):
         species = Species(None, acacia)
         _ = new_repo.add(species)
-        es.indices.refresh(index="species")
-        assert es.count(index="species")["count"] == 1
+        es.indices.refresh(index=new_repo.index)
+        assert es.count(index=new_repo.index)["count"] == 1
         new_repo.delete_all_documents()
-        es.indices.refresh(index="species")
-        assert es.count(index="species")["count"] == 0
+        es.indices.refresh(index=new_repo.index)
+        assert es.count(index=new_repo.index)["count"] == 0
