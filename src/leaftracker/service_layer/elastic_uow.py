eac3c965001be0811538cdd4fb53205c0be7d9a6
Ajahn Jhanarato <jhanarato@gmail.com>
2024-05-16 17:37:00 +0800

    Ignore unimplemented methods in ElasticUnitOfWork

--- src/leaftracker/service_layer/elastic_uow.py
+++ src/leaftracker/service_layer/elastic_uow.py
@@ -5,7 +5,7 @@ from leaftracker.adapters.elastic_repository import SpeciesRepository
 
 
 class ElasticUnitOfWork:
-    def __enter__(self) -> Self:
+    def __enter__(self) -> Self:  # type: ignore
         pass
 
     def __exit__(self, *args):
@@ -14,16 +14,16 @@ class ElasticUnitOfWork:
     def commit(self) -> None:
         pass
 
-    def committed(self) -> bool:
+    def committed(self) -> bool:  # type: ignore
         pass
 
     def rollback(self) -> None:
         pass
 
-    def batches(self) -> BatchRepository:
+    def batches(self) -> BatchRepository:  # type: ignore
         pass
 
-    def sources(self) -> SourceRepository:
+    def sources(self) -> SourceRepository:  # type: ignore
         pass
 
     def species(self) -> SpeciesRepository:
