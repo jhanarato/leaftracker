d1cb9e3d386ccc49cd8f6e361df4144b584cb3e2
Ajahn Jhanarato <jhanarato@gmail.com>
2024-05-26 10:16:24 +0800

    Remove test grouping by class.

--- test/test_elastic_uow.py
+++ test/test_elastic_uow.py
@@ -10,56 +10,61 @@ def uow() -> ElasticUnitOfWork:
     return uow
 
 
-class TestElasticUnitOfWork:
-    def test_should_rollback_if_not_committed(self, uow, saligna):
-        with uow:
-            uow.species().add(saligna)
+def test_should_rollback_if_not_committed(uow, saligna):
+    with uow:
+        uow.species().add(saligna)
 
-        assert saligna.reference is None
+    assert saligna.reference is None
 
-    def test_should_commit(self, uow, saligna):
-        with uow:
-            uow.species().add(saligna)
-            uow.commit()
 
-        assert saligna.reference is not None
+def test_should_commit(uow, saligna):
+    with uow:
+        uow.species().add(saligna)
+        uow.commit()
 
-    def test_should_add_two_species(self, uow, saligna, dentifera):
-        with uow:
-            uow.species().add(saligna)
-            uow.species().add(dentifera)
-            uow.commit()
+    assert saligna.reference is not None
 
-        assert uow.species().index.document_count() == 2
 
-    def test_should_clear_queue_on_rollback(self, uow, saligna):
-        with uow:
-            uow.species().add(saligna)
+def test_should_add_two_species(uow, saligna, dentifera):
+    with uow:
+        uow.species().add(saligna)
+        uow.species().add(dentifera)
+        uow.commit()
 
+    assert uow.species().index.document_count() == 2
+
+
+def test_should_clear_queue_on_rollback(uow, saligna):
+    with uow:
+        uow.species().add(saligna)
+
+    assert not uow.species().queued()
+
+
+def test_should_rollback_explicitly(uow, saligna):
+    with uow:
+        uow.species().add(saligna)
+        uow.rollback()
         assert not uow.species().queued()
 
-    def test_should_rollback_explicitly(self, uow, saligna):
-        with uow:
-            uow.species().add(saligna)
-            uow.rollback()
-            assert not uow.species().queued()
 
-    def test_should_clear_queue_after_commit(self, uow, saligna):
-        with uow:
-            uow.species().add(saligna)
-            uow.commit()
-            assert not uow.species().queued()
-
-    def test_should_get_all_indexes(self, uow):
-        index_names = [index.name for index in uow.indexes()]
-        assert index_names == ["test_species"]
+def test_should_clear_queue_after_commit(uow, saligna):
+    with uow:
+        uow.species().add(saligna)
+        uow.commit()
+        assert not uow.species().queued()
 
 
-class TestTestIndicies:
-    def test_should_normally_use_production_index(self):
-        uow = ElasticUnitOfWork(use_test_indexes=False)
-        assert uow.species().index.name == "species"
+def test_should_get_all_indexes(uow):
+    index_names = [index.name for index in uow.indexes()]
+    assert index_names == ["test_species"]
 
-    def test_should_use_test_index(self):
-        uow = ElasticUnitOfWork(use_test_indexes=True)
-        assert uow.species().index.name == "test_species"
+
+def test_should_normally_use_production_index():
+    uow = ElasticUnitOfWork(use_test_indexes=False)
+    assert uow.species().index.name == "species"
+
+
+def test_should_use_test_index():
+    uow = ElasticUnitOfWork(use_test_indexes=True)
+    assert uow.species().index.name == "test_species"
