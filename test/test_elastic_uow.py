44bd58dc6a1de241ce975d63954181fa110e61fe
Ajahn Jhanarato <jhanarato@gmail.com>
2024-05-26 10:13:47 +0800

    Split uow and repository tests into separate modules.

--- test/test_elastic_uow.py
+++ test/test_elastic_uow.py
@@ -1,28 +1,6 @@
 import pytest
 
-from leaftracker.adapters.elastic_repository import SpeciesRepository
 from leaftracker.service_layer.elastic_uow import ElasticUnitOfWork
-
-
-@pytest.fixture
-def repository() -> SpeciesRepository:
-    repo = SpeciesRepository(use_test_index=True)
-    repo.index.delete_all_documents()
-    return repo
-
-
-class TestSpeciesRepository:
-    def test_should_indicate_missing_document(self, repository):
-        assert repository.get("Nothing") is None
-
-    def test_should_queue_documents_to_commit(self, repository, saligna):
-        repository.add(saligna)
-        assert len(repository.queued()) == 1
-
-    def test_should_clear_queue(self, repository, saligna):
-        repository.add(saligna)
-        repository.clear_queue()
-        assert not repository.queued()
 
 
 @pytest.fixture
