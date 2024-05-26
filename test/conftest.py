3f61df31ff5af65c541f06c3281d4eb70e71fa74
Ajahn Jhanarato <jhanarato@gmail.com>
2024-05-26 10:45:22 +0800

    Move fakes to conftest.py

--- test/conftest.py
+++ test/conftest.py
@@ -1,7 +1,10 @@
+from typing import Self
+
 import pytest
 from elasticsearch import Elasticsearch
 
-from leaftracker.domain.model import Species, ScientificName
+from leaftracker.adapters.repository import BatchRepository, SourceRepository, SpeciesRepository
+from leaftracker.domain.model import Species, ScientificName, Batch, Source
 
 
 @pytest.fixture
@@ -31,3 +34,84 @@ def delete_test_indexes():
     yield
     es = Elasticsearch(hosts="http://localhost:9200")
     es.indices.delete(index=["test_index", "test_species"])
+
+
+class FakeBatchRepository:
+    def __init__(self, batches: list[Batch]):
+        self._next_batch_number = 1
+        self._batches = set(batches)
+
+    def _new_reference(self) -> str:
+        reference = f"batch-{self._next_batch_number:04}"
+        self._next_batch_number += 1
+        return reference
+
+    def add(self, batch: Batch) -> str:
+        batch.reference = self._new_reference()
+        self._batches.add(batch)
+        return batch.reference
+
+    def get(self, batch_ref: str) -> Batch | None:
+        matching = (batch for batch in self._batches
+                    if batch.reference == batch_ref)
+        return next(matching, None)
+
+
+class FakeSourceRepository:
+    def __init__(self, sources: list[Source]):
+        self._sources = set(sources)
+
+    def add(self, source: Source) -> str:
+        self._sources.add(source)
+        return source.name
+
+    def get(self, name: str) -> Source | None:
+        matching = (source for source in self._sources
+                    if source.name == name)
+        return next(matching, None)
+
+
+class FakeSpeciesRepository:
+    def __init__(self, species: list[Species]):
+        self._species = set(species)
+
+    def add(self, species: Species) -> str | None:
+        self._species.add(species)
+        return species.reference
+
+    def get(self, reference: str) -> Species | None:
+        matching = (species for species in self._species
+                    if species.reference == reference)
+        return next(matching, None)
+
+
+class FakeUnitOfWork:
+    def __init__(self):
+        self._batches: BatchRepository = FakeBatchRepository([])
+        self._sources: SourceRepository = FakeSourceRepository([])
+        self._species: SpeciesRepository = FakeSpeciesRepository([])
+        self._commited = False
+
+    def __enter__(self) -> Self:
+        return self
+
+    def __exit__(self, *args):
+        self.rollback()
+
+    def commit(self) -> None:
+        self._commited = True
+
+    def committed(self) -> bool:
+        return self._commited
+
+    def rollback(self) -> None:
+        pass
+
+    def batches(self) -> BatchRepository:
+        return self._batches
+
+    def sources(self) -> SourceRepository:
+        return self._sources
+
+    def species(self) -> SpeciesRepository:
+        return self._species
--- test/test_services.py
+++ test/test_services.py
@@ -1,61 +1,11 @@
-from typing import Self
-
 import pytest
 
-from leaftracker.adapters.repository import BatchRepository, SourceRepository, SpeciesRepository
-from leaftracker.domain.model import Batch, Source, SourceType, BatchType, Stock, StockSize, Species
+from conftest import FakeBatchRepository, FakeUnitOfWork
+from leaftracker.adapters.repository import BatchRepository
+from leaftracker.domain.model import Batch, Source, SourceType, BatchType, Stock, StockSize
 from leaftracker.service_layer import services
 from leaftracker.service_layer.services import InvalidSource
 from leaftracker.service_layer.unit_of_work import UnitOfWork
-
-
-class FakeBatchRepository:
-    def __init__(self, batches: list[Batch]):
-        self._next_batch_number = 1
-        self._batches = set(batches)
-
-    def _new_reference(self) -> str:
-        reference = f"batch-{self._next_batch_number:04}"
-        self._next_batch_number += 1
-        return reference
-
-    def add(self, batch: Batch) -> str:
-        batch.reference = self._new_reference()
-        self._batches.add(batch)
-        return batch.reference
-
-    def get(self, batch_ref: str) -> Batch | None:
-        matching = (batch for batch in self._batches
-                    if batch.reference == batch_ref)
-        return next(matching, None)
-
-
-class FakeSourceRepository:
-    def __init__(self, sources: list[Source]):
-        self._sources = set(sources)
-
-    def add(self, source: Source) -> str:
-        self._sources.add(source)
-        return source.name
-
-    def get(self, name: str) -> Source | None:
-        matching = (source for source in self._sources
-                    if source.name == name)
-        return next(matching, None)
-
-
-class FakeSpeciesRepository:
-    def __init__(self, species: list[Species]):
-        self._species = set(species)
-
-    def add(self, species: Species) -> str | None:
-        self._species.add(species)
-        return species.reference
-
-    def get(self, reference: str) -> Species | None:
-        matching = (species for species in self._species
-                    if species.reference == reference)
-        return next(matching, None)
 
 
 def test_fake_reference():
@@ -66,38 +16,6 @@ def test_fake_reference():
                   repo.add(Batch(Source("Natural Area", SourceType.NURSERY), BatchType.DELIVERY))]
 
     assert references == ["batch-0001", "batch-0002", "batch-0003"]
-
-
-class FakeUnitOfWork:
-    def __init__(self):
-        self._batches: BatchRepository = FakeBatchRepository([])
-        self._sources: SourceRepository = FakeSourceRepository([])
-        self._species: SpeciesRepository = FakeSpeciesRepository([])
-        self._commited = False
-
-    def __enter__(self) -> Self:
-        return self
-
-    def __exit__(self, *args):
-        self.rollback()
-
-    def commit(self) -> None:
-        self._commited = True
-
-    def committed(self) -> bool:
-        return self._commited
-
-    def rollback(self) -> None:
-        pass
-
-    def batches(self) -> BatchRepository:
-        return self._batches
-
-    def sources(self) -> SourceRepository:
-        return self._sources
-
-    def species(self) -> SpeciesRepository:
-        return self._species
 
 
 @pytest.fixture
