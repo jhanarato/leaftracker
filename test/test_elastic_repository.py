f473c1db860220a11cb76812a4bb2accb8470ec8
Ajahn Jhanarato <jhanarato@gmail.com>
2024-06-02 10:46:07 +0800

    Remove repository test. UoW tests should cover this.

--- test/test_elastic_repository.py
+++ test/test_elastic_repository.py
@@ -15,10 +15,6 @@ class TestSpeciesRepository:
     def test_should_indicate_missing_document(self, repository):
         assert repository.get("Nothing") is None
 
-    def test_should_queue_documents_to_commit(self, repository, saligna):
-        repository.add(saligna)
-        assert len(repository.added()) == 1
-
     def test_should_rollback(self, repository, saligna):
         repository.add(saligna)
         repository.rollback()
