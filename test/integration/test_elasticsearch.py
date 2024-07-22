f38e0bc3c794e9421f9e863b7222c8a398977f01
Ajahn Jhanarato <jhanarato@gmail.com>
2024-07-22 12:21:02 +0800

    Improve tests.

--- test/integration/test_elasticsearch.py
+++ test/integration/test_elasticsearch.py
@@ -55,21 +55,38 @@ class TestLifecycle:
 
 
 class TestDocumentStore:
-    def test_add_new_document(self, store, document_without_id):
+    def test_add_new_without_id(self, store, document_without_id):
         document_id = store.add(document_without_id)
         assert store.exists(document_id)
 
-    def test_get_existing_document(self, store, document_with_id):
+    def test_add_new_with_id(self, store, document_with_id):
+        existing_id = document_with_id.document_id
+        added_id = store.add(document_with_id)
+        assert existing_id == added_id
+        retrieved = store.get(existing_id)
+        assert retrieved.document_id == existing_id
+
+    def test_get_existing(self, store, document_with_id):
         store.add(document_with_id)
         retrieved = store.get(document_with_id.document_id)
         assert retrieved == document_with_id
 
-    def test_get_missing_document(self, store):
+    def test_get_missing(self, store):
         assert store.get("no-such-doc") is None
 
-    def test_document_doesnt_exist(self, store):
+    def test_doesnt_exist(self, store):
         assert not store.exists("not-a-doc")
 
-    def test_document_does_exist(self, store, document_with_id):
+    def test_does_exist(self, store, document_with_id):
         store.add(document_with_id)
         assert store.exists(document_with_id.document_id)
+
+    def test_update_existing(self, store, document_without_id):
+        store.add(document_without_id)
+        assert document_without_id.source["content"] == "some content"
+
+        document_without_id.source["content"] = "new content"
+        reference = store.add(document_without_id)
+
+        retrieved = store.get(reference)
+        assert retrieved.source["content"] == "new content"
