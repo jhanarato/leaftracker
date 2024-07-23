a12843ff58b4bce005be95c967db99f1ebda23ea
Ajahn Jhanarato <jhanarato@gmail.com>
2024-07-23 09:53:30 +0800

    Include lifecycle in test.

--- test/integration/test_elastic_repositories.py
+++ test/integration/test_elastic_repositories.py
@@ -1,17 +1,26 @@
 from leaftracker.adapters.elastic_repository import ElasticSpeciesRepository
-from leaftracker.adapters.elasticsearch import DocumentStore, Document
+from leaftracker.adapters.elasticsearch import DocumentStore, Document, Lifecycle
 from leaftracker.domain.model import Species
+from leaftracker.service_layer.elastic_uow import SPECIES_MAPPINGS
+
+INDEX = "test_integration_species"
 
 
 class TestElasticSpeciesRepository:
     def test_add_to_elasticsearch_index(self):
         species = Species(current_name="Machaerina juncea", reference="species-0001")
         species.taxon_history.add_previous_name("Baumea juncea")
-        store = DocumentStore("test_integration_species")
+
+        lifecycle = Lifecycle(INDEX, SPECIES_MAPPINGS)
+        lifecycle.create()
+
+        store = DocumentStore(INDEX)
         repository = ElasticSpeciesRepository(store)
         repository.add(species)
         repository.commit()
+
         document = store.get(species.reference)
+
         assert document == Document(
             document_id="species-0001",
             source={
@@ -19,3 +28,5 @@ class TestElasticSpeciesRepository:
                 "previous_scientific_names": ["Baumea juncea"]
             }
         )
+
+        lifecycle.delete()
