102d19a285831b4e6bd0c6c7f17fcb9e05badfb7
Ajahn Jhanarato <jhanarato@gmail.com>
2024-07-25 12:56:16 +0800

    Remove fixture.

--- test/unit/test_sources.py
+++ test/unit/test_sources.py
@@ -1,22 +1,15 @@
-import pytest
-
-from fakes import FakeUnitOfWork, FakeSourceOfStockRepository
+from fakes import FakeUnitOfWork
 from leaftracker.domain.model import SourceType
 from leaftracker.service_layer import services
 
 
-@pytest.fixture
-def uow() -> FakeUnitOfWork:
+def test_add_nursery():
     uow = FakeUnitOfWork()
-    uow._sources = FakeSourceOfStockRepository()
-    return uow
-
-
-def test_add_nursery(uow):
     reference = services.add_nursery("Trillion Trees", uow)
     assert uow.sources().get(reference).source_type == SourceType.NURSERY
 
 
-def test_add_program(uow):
+def test_add_program():
+    uow = FakeUnitOfWork()
     reference = services.add_program("Habitat Links", uow)
     assert uow.sources().get(reference).source_type == SourceType.PROGRAM
