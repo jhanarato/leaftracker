71aacf94e0759ad18465b922008e5d3da80d7382
Ajahn Jhanarato <jhanarato@gmail.com>
2024-07-28 15:20:13 +0800

    Tests check returned value is not None

--- test/unit/test_sources.py
+++ test/unit/test_sources.py
@@ -6,10 +6,14 @@ from leaftracker.service_layer import services
 def test_add_nursery():
     uow = FakeUnitOfWork()
     reference = services.add_nursery("Trillion Trees", uow)
-    assert uow.sources().get(reference).source_type == SourceType.NURSERY
+    retrieved = uow.sources().get(reference)
+    assert retrieved is not None
+    assert retrieved.source_type == SourceType.NURSERY
 
 
 def test_add_program():
     uow = FakeUnitOfWork()
     reference = services.add_program("Habitat Links", uow)
-    assert uow.sources().get(reference).source_type == SourceType.PROGRAM
+    retrieved = uow.sources().get(reference)
+    assert retrieved is not None
+    assert retrieved.source_type == SourceType.PROGRAM
