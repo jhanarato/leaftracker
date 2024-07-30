94073442a321516daab883b283cf9901f2d577bd
Ajahn Jhanarato <jhanarato@gmail.com>
2024-07-30 12:09:20 +0800

    Add integration test for ElasticSourceOfStockRepository

--- test/integration/test_elastic_repositories.py
+++ test/integration/test_elastic_repositories.py
@@ -1,6 +1,6 @@
-from leaftracker.adapters.elastic_repository import ElasticSpeciesRepository
+from leaftracker.adapters.elastic_repository import ElasticSpeciesRepository, ElasticSourceOfStockRepository
 from leaftracker.adapters.elasticsearch import DocumentStore, Document, Lifecycle
-from leaftracker.domain.model import Species
+from leaftracker.domain.model import Species, SourceOfStock, SourceType
 from leaftracker.service_layer.elastic_uow import SPECIES_MAPPINGS, SPECIES_INDEX, SOURCE_OF_STOCK_INDEX, \
     SOURCE_OF_STOCK_MAPPINGS
 
@@ -37,4 +37,22 @@ class TestElasticSourceOfStockRepository:
     def test_add_to_source_of_stock_index(self):
         lifecycle = Lifecycle(INDEX_PREFIX + SOURCE_OF_STOCK_INDEX, SOURCE_OF_STOCK_MAPPINGS)
         lifecycle.create()
+
+        source = SourceOfStock("Trillion Trees", SourceType.NURSERY, "source-0001")
+
+        store = DocumentStore(INDEX_PREFIX + SOURCE_OF_STOCK_INDEX)
+        repository = ElasticSourceOfStockRepository(store)
+        repository.add(source)
+        repository.commit()
+
+        document = store.get(source.reference)
+
+        assert document == Document(
+            document_id="source-0001",
+            source={
+                "current_name": "Trillion Trees",
+                "source_type": "nursery",
+            }
+        )
+
         lifecycle.delete()
