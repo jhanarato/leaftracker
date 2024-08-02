019d10cee9214fe3c489894bcbde4d75938fe420
Ajahn Jhanarato <jhanarato@gmail.com>
2024-08-02 12:51:06 +0800

    Delete tests.

--- test/unit/test_fake_persistence.py
+++ test/unit/test_fake_persistence.py
@@ -1,44 +1,5 @@
-from fakes import FakeLifecycle, FakeDocumentStore, FakeSpeciesRepository
+from fakes import FakeLifecycle, FakeDocumentStore
 from leaftracker.adapters.elasticsearch import Document
-
-
-class TestFakeUnitOfWork:
-    def test_should_rollback_if_not_committed(self, uow, saligna):
-        with uow:
-            uow.species().add(saligna)
-
-        assert saligna.reference is None
-
-    def test_should_assign_reference_on_commit(self, uow, saligna):
-        with uow:
-            uow.species().add(saligna)
-            uow.commit()
-
-        assert saligna.reference == "species-0001"
-
-    def test_should_retrieve_species_after_commit(self, uow, saligna):
-        with uow:
-            uow.species().add(saligna)
-            uow.commit()
-
-        retrieved = uow.species().get(saligna.reference)
-        assert retrieved.reference == "species-0001"  # type: ignore
-
-    def test_should_discard_uncommitted_species_on_rollback(self, uow, saligna, dentifera):
-        with uow:
-            uow.species().add(saligna)
-
-        with uow:
-            uow.species().add(dentifera)
-            uow.commit()
-
-        retrieved = uow.species().get("species-0001")
-        assert retrieved.taxon_history.current().species == "dentifera"  # type: ignore
-
-
-class TestFakeSpeciesRepository:
-    def test_should_return_none_when_missing(self):
-        assert FakeSpeciesRepository().get("missing") is None
 
 
 class TestFakeLifecycle:
