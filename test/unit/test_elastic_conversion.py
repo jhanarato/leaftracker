392a54910f9b7d237c08da51b7251257841f72df
Ajahn Jhanarato <jhanarato@gmail.com>
2024-08-07 12:40:50 +0800

    Extract fixtures.

--- test/unit/test_elastic_conversion.py
+++ test/unit/test_elastic_conversion.py
@@ -5,24 +5,31 @@ from leaftracker.adapters.elastic.elasticsearch import Document
 from leaftracker.domain.model import Batch, BatchType, Stock, StockSize
 
 
+@pytest.fixture
+def batch() -> Batch:
+    batch = Batch("source_of_stock-xyz", BatchType.PICKUP, "batch-xyz")
+    batch.add(Stock("species-xyz", 20, StockSize.TUBE))
+    return batch
+
+
+@pytest.fixture
+def batch_document() -> Document:
+    return Document(
+        document_id="batch-xyz",
+        source={
+            "source_reference": "source_of_stock-xyz",
+            "batch_type": "pickup",
+            "stock": [
+                {
+                    "species_reference": "species-xyz",
+                    "quantity": 20,
+                    "size": "tube",
+                }
+            ]
+        }
+    )
+
+
 class TestConvertBatch:
-    def test_batch_with_stock(self):
-        batch = Batch("source_of_stock-xyz", BatchType.PICKUP, "batch-xyz")
-        batch.add(Stock("species-xyz", 20, StockSize.TUBE))
-
-        document = batch_to_document(batch)
-
-        assert document == Document(
-            document_id="batch-xyz",
-            source={
-                "source_reference": "source_of_stock-xyz",
-                "batch_type": "pickup",
-                "stock": [
-                    {
-                        "species_reference": "species-xyz",
-                        "quantity": 20,
-                        "size": "tube",
-                    }
-                ]
-            }
-        )
+    def test_with_one_stock(self, batch, batch_document):
+        assert batch_to_document(batch) == batch_document
