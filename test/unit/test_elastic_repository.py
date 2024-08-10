8020046c941dac65dc5ee0e214ab950490577048
Ajahn Jhanarato <jhanarato@gmail.com>
2024-08-10 12:23:49 +0800

    Move tests to new module.

--- test/unit/test_elastic_repository.py
+++ test/unit/test_elastic_repository.py
@@ -3,46 +3,6 @@ import pytest
 from fakes import FakeDocumentStore
 from leaftracker.adapters.elastic.elasticsearch import Document
 from leaftracker.adapters.elastic.aggregate_io import AggregateWriter, AggregateReader
-from leaftracker.domain.model import Batch, BatchType, Stock, StockSize
-
-
-@pytest.fixture
-def batch_aggregate() -> Batch:
-    batch = Batch("source-0001", BatchType.PICKUP, "batch-0001")
-    batch.add(Stock("species-0001", 20, StockSize.TUBE))
-    batch.add(Stock("species-0002", 5, StockSize.POT))
-    return batch
-
-
-@pytest.fixture
-def batch_aggregate_2() -> Batch:
-    batch = Batch("source-0001", BatchType.PICKUP, "batch-0002")
-    batch.add(Stock("species-0001", 20, StockSize.TUBE))
-    batch.add(Stock("species-0002", 5, StockSize.POT))
-    return batch
-
-
-@pytest.fixture
-def batch_document():
-    return Document(
-        document_id="batch-0001",
-        source={
-            "source_reference": "source-0001",
-            "batch_type": "pickup",
-            "stock": [
-                {
-                    "species_reference": "species-0001",
-                    "quantity": 20,
-                    "size": "tube",
-                },
-                {
-                    "species_reference": "species-0002",
-                    "quantity": 5,
-                    "size": "pot",
-                },
-            ]
-        }
-    )
 
 
 class Aggregate:
