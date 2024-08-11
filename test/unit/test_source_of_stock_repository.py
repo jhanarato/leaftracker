038c782bd23281d559785d181c45c6a41a6d127e
Ajahn Jhanarato <jhanarato@gmail.com>
2024-08-11 09:28:23 +0800

    Add test.

--- test/unit/test_source_of_stock_repository.py
+++ test/unit/test_source_of_stock_repository.py
@@ -32,3 +32,13 @@ class TestElasticSourceOfStockRepository:
 
         stored = source_store.get(document.document_id)
         assert stored == document
+
+    def test_get(self, uow, source_store, document):
+        source_store.add(document)
+
+        with uow:
+            source = uow.sources().get(document.document_id)
+
+        assert source.reference == "source_of_stock-0001"
+        assert source.source_type == SourceType.NURSERY
+        assert source.current_name == "Trillion Trees"
