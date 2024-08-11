8d51fbfc56b469da9b241713ddf887cb7cd92bab
Ajahn Jhanarato <jhanarato@gmail.com>
2024-08-11 10:04:43 +0800

    Rewrite batch repository tests.

--- test/unit/test_batch_repository.py
+++ test/unit/test_batch_repository.py
@@ -6,7 +6,49 @@ from leaftracker.domain.model import Batch, BatchType, Stock, StockSize
 
 
 @pytest.fixture
-def batch() -> Batch:
+def batch_with_none() -> Batch:
+    return Batch("source-0001", BatchType.PICKUP, "batch-0001")
+
+
+@pytest.fixture
+def document_with_none() -> Document:
+    return Document(
+        document_id="batch-0001",
+        source={
+            "source_reference": "source-0001",
+            "batch_type": "pickup",
+            "stock": []
+        }
+    )
+
+
+@pytest.fixture
+def batch_with_one() -> Batch:
+    batch = Batch("source-0001", BatchType.PICKUP, "batch-0001")
+    batch.add(Stock("species-0001", 20, StockSize.TUBE))
+    return batch
+
+
+@pytest.fixture
+def document_with_one():
+    return Document(
+        document_id="batch-0001",
+        source={
+            "source_reference": "source-0001",
+            "batch_type": "pickup",
+            "stock": [
+                {
+                    "species_reference": "species-0001",
+                    "quantity": 20,
+                    "size": "tube",
+                },
+            ]
+        }
+    )
+
+
+@pytest.fixture
+def batch_with_two() -> Batch:
     batch = Batch("source-0001", BatchType.PICKUP, "batch-0001")
     batch.add(Stock("species-0001", 20, StockSize.TUBE))
     batch.add(Stock("species-0002", 5, StockSize.POT))
@@ -14,22 +56,7 @@ def batch() -> Batch:
 
 
 @pytest.fixture
-def batch_2() -> Batch:
-    batch = Batch("source-0001", BatchType.PICKUP, "batch-0002")
-    batch.add(Stock("species-0001", 20, StockSize.TUBE))
-    batch.add(Stock("species-0002", 5, StockSize.POT))
-    return batch
-
-
-@pytest.fixture
-def batch_3() -> Batch:
-    batch = Batch("source_of_stock-xyz", BatchType.PICKUP, "batch-xyz")
-    batch.add(Stock("species-xyz", 20, StockSize.TUBE))
-    return batch
-
-
-@pytest.fixture
-def document_1():
+def document_with_two() -> Document:
     return Document(
         document_id="batch-0001",
         source={
@@ -51,28 +78,22 @@ def document_1():
     )
 
 
-@pytest.fixture
-def document_3() -> Document:
-    return Document(
-        document_id="batch-xyz",
-        source={
-            "source_reference": "source_of_stock-xyz",
-            "batch_type": "pickup",
-            "stock": [
-                {
-                    "species_reference": "species-xyz",
-                    "quantity": 20,
-                    "size": "tube",
-                }
-            ]
-        }
-    )
+class TestElasticBatchRepository:
+    def test_add_batch_with_no_stock(self, uow, batch_store, batch_with_none, document_with_none):
+        with uow:
+            uow.batches().add(batch_with_none)
+            uow.commit()
 
+        stored = batch_store.get(document_with_none.document_id)
 
-class TestConvertBatch:
-    def test_with_stock_to_document(self, batch_3, document_3):
-        assert batch_to_document(batch_3) == document_3
+        assert stored == document_with_none
 
-    def test_from_document_with_stock(self, batch_3, document_3):
-        batch = document_to_batch(document_3)
-        assert batch.quantity("species-xyz") == 20
+    def test_get_batch_with_no_stock(self, uow, batch_store, document_with_none):
+        batch_store.add(document_with_none)
+
+        with uow:
+            batch = uow.batches().get(document_with_none.document_id)
+
+        assert batch.reference == "batch-0001"
+        assert batch.batch_type == BatchType.PICKUP
+        assert batch.stock == []
