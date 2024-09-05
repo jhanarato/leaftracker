c9e0cde2fdd67979cac751978d5312e2d9357d04
Ajahn Jhanarato <jhanarato@gmail.com>
2024-09-05 09:10:11 +0800

    Add main() function and create species index.

--- src/leaftracker/adapters/gnome_ui/app.py
+++ src/leaftracker/adapters/gnome_ui/app.py
@@ -1,5 +1,9 @@
 import gi
 
+from integration.test_elasticsearch import lifecycle
+from leaftracker.adapters.elastic.elasticsearch import Lifecycle
+from leaftracker.adapters.elastic.repositories.species import SPECIES_INDEX, SPECIES_MAPPINGS
+
 gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
 from gi.repository import Adw, Gtk
@@ -7,11 +11,21 @@ from gi.repository import Adw, Gtk
 from leaftracker.adapters.gnome_ui.species import SpeciesDialog
 
 
+def create_indexes():
+    Lifecycle(SPECIES_INDEX, SPECIES_MAPPINGS).create()
+
+
 def on_activate(app):
     species_dialog = SpeciesDialog(application=app)
     species_dialog.present()
 
 
-app = Adw.Application(application_id='org.bswa.jhanarato.LeafTracker')
-app.connect('activate', on_activate)
-app.run(None)
+def main():
+    create_indexes()
+    app = Adw.Application(application_id='org.bswa.jhanarato.LeafTracker')
+    app.connect('activate', on_activate)
+    app.run(None)
+
+
+if __name__ == "__main__":
+    main()
