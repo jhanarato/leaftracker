fed99533b64c6acc7712cba34c732a4333204deb
Ajahn Jhanarato <jhanarato@gmail.com>
2024-09-08 18:33:50 +0800

    Flatpack build displays a window. Wow that was hard.

--- leaftracker-gtk/src/main.py
+++ leaftracker-gtk/src/main.py
@@ -18,59 +18,19 @@
 import sys
 import gi
 
-gi.require_version('Gtk', '4.0')
+# gi.require_version('Gtk', '4.14')
+gi.require_version('Adw', '1')
 
-from gi.repository import Gtk, Gio
-from .window import LeaftrackerGtkWindow, AboutDialog
+from gi.repository import Adw, Gtk, Gio
+from .window_test import WindowTest
 
 
-class Leaftracker_gtkApplication(Gtk.Application):
-    """The main application singleton class."""
-
-    def __init__(self):
-        super().__init__(application_id='org.bswa.jhanarato.Leaftracker',
-                         flags=Gio.ApplicationFlags.FLAGS_NONE)
-        self.create_action('quit', self.quit, ['<primary>q'])
-        self.create_action('about', self.on_about_action)
-        self.create_action('preferences', self.on_preferences_action)
-
-    def do_activate(self):
-        """Called when the application is activated.
-
-        We raise the application's main window, creating it if
-        necessary.
-        """
-        win = self.props.active_window
-        if not win:
-            win = LeaftrackerGtkWindow(application=self)
-        win.present()
-
-    def on_about_action(self, widget, _):
-        """Callback for the app.about action."""
-        about = AboutDialog(self.props.active_window)
-        about.present()
-
-    def on_preferences_action(self, widget, _):
-        """Callback for the app.preferences action."""
-        print('app.preferences action activated')
-
-    def create_action(self, name, callback, shortcuts=None):
-        """Add an application action.
-
-        Args:
-            name: the name of the action
-            callback: the function to be called when the action is
-              activated
-            shortcuts: an optional list of accelerators
-        """
-        action = Gio.SimpleAction.new(name, None)
-        action.connect("activate", callback)
-        self.add_action(action)
-        if shortcuts:
-            self.set_accels_for_action(f"app.{name}", shortcuts)
+def on_activate(app):
+    window = WindowTest(application=app)
+    window.present()
 
 
 def main(version):
-    """The application's entry point."""
-    app = Leaftracker_gtkApplication()
+    app = Adw.Application(application_id='org.bswa.jhanarato.Leaftracker')
+    app.connect('activate', on_activate)
     return app.run(sys.argv)
